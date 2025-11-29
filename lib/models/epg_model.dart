cd4b0b9dae1f99f0d0fbdf30b0bce08b58426753
mednet <logiaconsultingcontact@gmail.com>
2025-11-29 16:03:07 +0100

    epg_model.dart

---
+++ lib/models/epg_model.dart
@@ -0,0 +1,13 @@
+class EPGProgram {
+  final String title;
+  final String description;
+  final String start;
+  final String end;
+
+  EPGProgram({
+    required this.title,
+    required this.description,
+    required this.start,
+    required this.end,
+  });
+}
